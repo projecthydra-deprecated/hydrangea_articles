2d5aee2c7159ca009ccfb777c7edc813d8022745
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-04-26 01:44:54 -0500

    (Hydrus) HYDRUS-157
    set up views to support adding contributors without javascript
    added some code comments to contributors_controller
    made contributors_controller support .inline format

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -5,19 +5,36 @@ class ContributorsController < ApplicationController
   include MediaShelf::ActiveFedoraHelper
   before_filter :require_solr, :require_fedora
   
+  # If contributor_type is provided, renders the appropriate "new" form
+  # If contributor_type is not provided, renders a form for selecting which type of contributor to add
+  # If format is .inline, this renders without layout so you can embed it in a page
   def new
-    render :partial=>"contributors/new"
+    
+    # Only load the document if you need to
+    if params.has_key?(:contributor_type) 
+      @document_fedora = load_document_from_id(params[:asset_id])
+      @next_contributor_index = @document_fedora
+    end
+    
+    respond_to do |format|
+      format.html { render :file=>"contributors/new.html" , :layout=>true}
+      format.inline { render :partial=>"contributors/new.html", :layout=>false }
+    end
   end
   
   def create
-    af_model = retrieve_af_model(params[:content_type], :default=>HydrangeaArticle)
-    @document_fedora = af_model.find(params[:asset_id])
+    @document_fedora = load_document_from_id(params[:asset_id])
     
     ct = params[:contributor_type]
     inserted_node, new_node_index = @document_fedora.insert_contributor(ct)
     @document_fedora.save
-    partial_name = "contributors/edit_#{ct}"
-    render :partial=>partial_name, :locals=>{"edit_#{ct}".to_sym =>inserted_node, "edit_#{ct}_counter".to_sym =>new_node_index}, :layout=>false
+    debugger
+    partial_name = "contributors/edit_#{ct}.html"
+    respond_to do |format|
+      format.html { redirect_to( url_for(:controller=>"catalog", :action=>"edit", :id=>params[:asset_id] )+"##{params[:contributor_type]}_#{new_node_index}" ) }
+      format.inline { render :partial=>partial_name, :locals=>{"edit_#{ct}".to_sym =>inserted_node, "edit_#{ct}_counter".to_sym =>new_node_index}, :layout=>false }
+    end
+    
   end
   
   def destroy
@@ -28,4 +45,10 @@ class ContributorsController < ApplicationController
     render :text=>result.inspect
   end
   
+  private
+  
+  def load_document_from_id(asset_id)
+    af_model = retrieve_af_model(params[:content_type], :default=>HydrangeaArticle)
+    af_model.find(asset_id)
+  end
 end
